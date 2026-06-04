eb82b74e99b7ff0078457e001c9c191b830f7b38
Timo Njagi <timothynjihia@gmail.com>
2026-06-04 10:40:35 +0300

    Revert overlayscrollbars to v1 (v2 incompatible with overlayscrollbars-react@0.3)
    - overlayscrollbars-react@0.3.0 requires overlayscrollbars@^1.10.0
    - v2 removed default export, causing client-side crash
    - Reverts CSS import path back to overlayscrollbars/css/OverlayScrollbars.css

--- packages/medsy-classic/src/pages/_app.tsx
+++ packages/medsy-classic/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import 'react-spring-modal/dist/index.css';
 import 'rc-collapse/assets/index.css';
-import 'overlayscrollbars/styles/overlayscrollbars.css';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
 import 'react-multi-carousel/lib/styles.css';
 
 import 'assets/styles/index.css';
